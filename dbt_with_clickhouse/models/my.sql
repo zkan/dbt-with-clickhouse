c7fb6f5fcf531455db910ba00280a9c3ca971f30
zkan <kan@odds.team>
2025-03-08 21:10:57 +0700

    Test distributed table

---
+++ dbt_with_clickhouse/models/my.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        materialized="distributed_table",
+        order_by="id"
+    )
+}}
+
+with source_data as (
+
+    select 1 as id
+    union all
+    select 2 as id
+    union all
+    select 3 as id
+    union all
+    select 4 as id
+    union all
+    select 5 as id
+
+)
+
+select *
+from source_data
